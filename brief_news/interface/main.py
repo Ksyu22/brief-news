33baee1b19121957eddf05204d6ea5fe35a4042f
Oksana Mishchenko <ksyu.ha@hotmail.com>
2022-11-26 14:58:08 +0100

    changing package name to brief_news

---
+++ brief_news/interface/main.py
@@ -0,0 +1,23 @@
+from brief_news.data.news_links import get_urls, get_urls_for_categories, get_headlines_for_categories, get_list_of_API_news_providers
+from brief_news.data.scraper import General_scraper, CNN_scraper
+
+import pandas as pd
+
+# def getting_url_category(category, country):
+#     url_list = get_headlines_for_categories(category, country)
+#     return url_list
+
+def get_articles(keyword):
+
+    urls = get_urls(keyword)
+    articles = [CNN_scraper(url) for url in urls]
+    df = pd.DataFrame(articles)
+
+    return df
+
+test = get_articles('business')
+print(test)
+
+if __name__ == '__main__':
+    print('ok')
+    #get_articles('business', 'us')
