bb2f701d8d1c4292499e52c36a7ca2bd15e5d276
Oksana Mishchenko <ksyu.ha@hotmail.com>
2022-11-26 17:06:13 +0100

    adding transformer

--- brief_news/interface/main.py
+++ brief_news/interface/main.py
@@ -1,11 +1,9 @@
 from brief_news.data.news_links import get_urls, get_urls_for_categories, get_headlines_for_categories, get_list_of_API_news_providers
 from brief_news.data.scraper import General_scraper, CNN_scraper
 
+from brief_news.ml_logic.transformer_model import summary_t5_small
 import pandas as pd
 
-# def getting_url_category(category, country):
-#     url_list = get_headlines_for_categories(category, country)
-#     return url_list
 
 def get_articles(keyword):
 
@@ -15,8 +13,19 @@ def get_articles(keyword):
 
     return df
 
-test = get_articles('business')
-print(test)
+
+def transfomer_summaries(data_frame):
+
+    data_point = data_frame['article']
+
+    summary = summary_t5_small(data_point)
+
+    return
+
+test_df, columns_df = get_articles('business')
+sum = transfomer_summaries(test_df)
+
+print(sum)
 
 if __name__ == '__main__':
     print('ok')
--- brief_news/ml_logic/transformer_model.py
+++ brief_news/ml_logic/transformer_model.py
@@ -5,12 +5,12 @@ from transformers import pipeline
 
 def summary_t5_small(articles_list: pd.DataFrame) -> pd.DataFrame:
 
-    """ this function download the t5-small summarization model and
+    """ This function downloads the t5-small summarization model and
     infer the summary"""
 
     summarization = pipeline(task='summarization')
 
-    article=articles_list['articles'][0]
+    article=articles_list[0]
     result = summarization([article])
 
     return result
